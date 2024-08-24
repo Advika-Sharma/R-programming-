62463dccbf122903a687c7bea0e6969c4b5d1d6d
advika sharmna <sharmaadvikaa@gmail.com>
2024-08-24 17:40:29 +0530

    experiments on DV-R

---
+++ data_analysis.R
@@ -0,0 +1,30 @@
+"hello world"
+5+5
+# this is a comment 
+for (x in 1:10) {
+  print(x)
+}
+
+name <- "John"
+age <- 40
+
+name  
+age 
+
+text1 <- "R is"
+text2 <- "awesome"
+
+paste(text1, text2)
+
+num1 <- 5
+num2 <- 10
+
+num1 + num2
+
+# Assign the same value to multiple variables in one line
+var1 <- var2 <- var3 <- "Orange"
+
+# Print variable values
+var1
+var2
+var3
