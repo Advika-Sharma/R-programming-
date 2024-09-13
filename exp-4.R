3b61d6cf4546ec04a2000c3101eb9d88c47f8570
advika sharmna <sharmaadvikaa@gmail.com>
2024-09-13 10:47:47 +0530

    import export data from file and user

--- exp-4.R
+++ exp-4.R
@@ -1,1 +1,9 @@
 #Clean and wrangle data to remove errors and inconsistencies.
+#Import and export data from various sources.
+#availabe.packages()
+
+#taking data from the system 
+library(tidyverse)
+library(readr)
+data1<-read.csv("C:\\Users\\Advika Sharma\\Desktop\\R\\PLACES__Local_Data_for_Better_Health__County_Data_2024_release.csv", header = TRUE)
+head(data1)
--- exp3-B.R
+++ exp3-B.R
@@ -4,13 +4,22 @@
 #taking data from the system 
 library(tidyverse)
 library(readr)
-data1<-read.csv("C:\\Users\\Advika Sharma\\Desktop\\R\\PLACES__Local_Data_for_Better_Health__County_Data_2024_release.csv", header = TRUE)
-head(data1)
+#data1<-read.csv("C:\\Users\\Advika Sharma\\Desktop\\R\\PLACES__Local_Data_for_Better_Health__County_Data_2024_release.csv", header = TRUE)
+#head(data1)
 
 #creating your own data 
-data.frame("name"=c("soni","ankit","adam","eve"),
+df <- data.frame("name"=c("soni","ankit","adam","eve"),
 "age"=c(25,30,35,40),
 "language"=c("java","python","R","C++"),
 "branch"=c("CS","DS","AI","IOT"))
 
+#exporting 
+#exporting data to csv file
+#write.csv(data1,"C:\\Users\\Advika Sharma\\Desktop\\R\\data.csv")
 
+#exporting own data 
+write.table(df,
+file = "C:\\Users\\Advika Sharma\\Desktop\\R\\student.txt",
+sep = "\t",
+row.names = TRUE,
+col.names = NA)
