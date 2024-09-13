82e04625fec76fbfd7a28160b63831c62749845b
advika sharmna <sharmaadvikaa@gmail.com>
2024-09-13 17:40:34 +0530

    data

--- exp-4.R
+++ exp-4.R
@@ -1,13 +1,16 @@
 #Clean and wrangle data to remove errors and inconsistencies.
-#Import and export data from various sources.
-#availabe.packages()
 
 #taking data from the system 
 library(tidyverse)
 library(readr)
-data1<-read.csv("C:\\Users\\Advika Sharma\\Desktop\\R\\PLACES__Local_Data_for_Better_Health__County_Data_2024_release.csv", header = TRUE)
-head(data1)
+data1<-read.csv("C:\\Users\\Advika Sharma\\Desktop\\R\\PLACES__Local_Data_for_Better_Health__County_Data_2024_release.csv", header = TRUE) 
+#head(data1)
 
-summary(data1)
+data1 <- data1[, -12]
+View(data1)
+data1 <- data1[, -11]
+View(data1)
+data1 <- data1[, -20]
+View(data1)
 
-
+write.csv(data1,"C:\\Users\\Advika Sharma\\Desktop\\data.csv")
--- tempCodeRunnerFile.R
+++ tempCodeRunnerFile.R
@@ -1,1 +1,1 @@
-print(mean(mtcars))
+#write.csv(data1,"C:\\Users\\Advika Sharma\\Desktop\\R\\data.csv")
